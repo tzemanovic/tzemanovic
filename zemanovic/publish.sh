137af610235582245e8fa7ce947fc3ff7314a79c
Tomas Zemanovic <tzemanovic@gmail.com>
2014-04-30 08:40:21 +0100

    msg

--- zemanovic/publish.sh
+++ zemanovic/publish.sh
@@ -2,18 +2,28 @@
 mkdir -p _publish
 cd _publish
 if [ ! -d "maseek.github.io" ]; then
-    echo "cloning"
     git clone https://github.com/maseek/maseek.github.io.git
+else
+	git pull origin master
 fi
 cd maseek.github.io
-echo "copying data"
+mv CNAME .CNAME
+mv README.md .README.md
 rm -rf *
+mv .CNAME CNAME
+mv .README.md README.md
 cp -rf ../../_site/* .
 git add --all .
 git add -u :/
-echo "commiting"
-git commit -m "publishing"
-echo "pushing"
+echo -n "enter commit message: "
+read msg
+git commit -m msg
 # use this first if on cygwin
 # git config --global core.askpass "git-gui--askpass"
+git push origin master
+
+cd ../..
+git add -all .
+git add -u :/
+git commit -m msg
 git push origin master
--- zemanovic/site.hs
+++ zemanovic/site.hs
@@ -1,23 +1,17 @@
+-- some parts inspired by <https://github.com/yogsototh/yblog>
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import Data.Monoid (mappend)
+import Data.Monoid ((<>))
+import System.FilePath.Posix (takeBaseName,takeDirectory,(</>),splitFileName)
+import Data.List (isInfixOf)
 import Hakyll
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match ("favicon.ico" .||. "robots.txt" .||. "images/*" .||. "css/style.min.css") $ do
+    match ("favicon.ico" .||. "robots.txt" .||. "images/**" .||. "css/*") $ do
         route   idRoute
         compile copyFileCompiler
-
-    {-match "css/*" $ compile getResourceBody
-
-    create ["style.css"] $ do
-        route   idRoute
-        compile $ do
-            items <- loadAll "css/*"
-            makeItem $ concatMap itemBody (items :: [Item String])
-                >>= compressCss . return-}
 
     match "js/*" $ do
         route idRoute
@@ -30,19 +24,20 @@ main = hakyll $ do
             >>= relativizeUrls
 
     match "posts/*" $ do
-        route $ setExtension "html"
+        route $ niceRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
+            >>= removeIndexHtml
 
     create ["archive.html"] $ do
         route idRoute
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "archives"            `mappend`
+                    listField "posts" postCtx (return posts) <>
+                    constField "title" "archives"            <>
                     defaultContext
 
             makeItem ""
@@ -56,46 +51,75 @@ main = hakyll $ do
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "active-home" "y"             `mappend`
-                    constField "title" "home"                `mappend`
+                    listField "posts" postCtx (return posts) <>
+                    constField "active-home" "y"             <>
+                    constField "title" "home"                <>
                     defaultContext
             getResourceBody
                 >>= applyAsTemplate indexCtx
                 >>= loadAndApplyTemplate "templates/default.html" indexCtx
                 >>= relativizeUrls
+                >>= removeIndexHtml
 
     match "blog.html" $ do
-        route idRoute
+        route niceRoute
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let blogCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "active-blog" "y"             `mappend`
+                    listField "posts" postCtx (return posts) <>
+                    constField "active-blog" "y"             <>
                     defaultContext
             getResourceBody
                 >>= applyAsTemplate blogCtx
                 >>= loadAndApplyTemplate "templates/default.html" blogCtx
                 >>= relativizeUrls
+                >>= removeIndexHtml
 
     match "projects.html" $ do
-        route idRoute
+        route niceRoute
         compile $ do
             let projectsCtx =
-                    constField "active-projects" "y"        `mappend`
-                    constField "title" "projects"           `mappend`
+                    constField "active-projects" "y"        <>
+                    constField "title" "projects"           <>
                     defaultContext
             getResourceBody
                 >>= applyAsTemplate projectsCtx
                 >>= loadAndApplyTemplate "templates/default.html" projectsCtx
                 >>= relativizeUrls
+                >>= removeIndexHtml
 
     match "templates/*" $ compile templateCompiler
-
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    constField "active-blog" "y" `mappend`
+    dateField "date" "%B %e, %Y"    <>
+    constField "active-blog" "y"    <>
+    metaKeywordCtx                  <>
     defaultContext
+
+--------------------------------------------------------------------------------
+metaKeywordCtx :: Context String
+metaKeywordCtx = field "metaKeywords" $ \item -> do
+    tags <- getMetadataField (itemIdentifier item) "tags"
+    return $ maybe "" showMetaTags tags
+        where
+            showMetaTags t = "<meta name=\"keywords\" content=\"" ++ t ++ "\"/>\n"
+
+--------------------------------------------------------------------------------
+niceRoute :: Routes
+niceRoute = customRoute createIndexRoute
+    where
+        createIndexRoute ident =
+            takeDirectory p </> takeBaseName p </> "index.html"
+            where p = toFilePath ident
+
+--------------------------------------------------------------------------------
+removeIndexHtml :: Item String -> Compiler (Item String)
+removeIndexHtml item = return $ fmap (withUrls removeIndexStr) item
+    where
+        removeIndexStr :: String -> String
+        removeIndexStr url = case splitFileName url of
+            (dir, "index.html") | isLocal dir -> dir
+            _                                 -> url
+            where isLocal uri = not (isInfixOf "://" uri)
